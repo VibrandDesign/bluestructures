a1e223efb641da6d976a0cb3498a311b4545816d
Federico Valla <38297526+vallafederico@users.noreply.github.com>
2025-06-17 13:03:07 +0200

    adds subscribers

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,25 @@
 import { createModules } from "./modules/_/create";
+import { Raf, Resize } from "@lib/subs";
 
 class App {
   constructor() {
     console.log("app.js/ts::", performance.now());
     this.init();
+
+    Raf.add(this.update.bind(this));
+    Resize.add(this.resize.bind(this));
   }
 
   init() {
     const modules = createModules();
+  }
+
+  update(data: any) {
+    // console.log("update", data);
+  }
+
+  resize(data: any) {
+    console.log("resize", data);
   }
 }
 
