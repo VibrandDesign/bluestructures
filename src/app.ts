f9808a9980d87a092e08bb6000523c480e700672
Federico Valla <38297526+vallafederico@users.noreply.github.com>
2025-08-25 19:45:40 +0200

    bumpity

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,19 @@ import { Pages } from "@lib/pages";
 
 // history.scrollRestoration = "manual";
 
+const IMG_URL =
+  "https://webflow-dev-setup-xi.vercel.app/public/bd.002_Bake6_CyclesBake_COMBINED.png";
+
 class _App {
   private scroll = Scroll;
   pages = Pages;
 
-  constructor() {}
+  constructor() {
+    const img = document.createElement("img");
+    img.src = IMG_URL;
+    console.log(img);
+    document.body.appendChild(img);
+  }
 }
 
 export const App = new _App();
